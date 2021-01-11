12625c158fd78417e9430d815077468357c8d38c
Mathilde Delanoe <openclassrooms@dlne.eu>
2021-01-11 15:40:17 +0100

    Implementation de la route post pour liker/disliker les sauces

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,5 +1,4 @@
 // Import du modèle
-const { JsonWebTokenError } = require('jsonwebtoken');
 const sauce = require('../models/sauce');
 
 // Gestion de l'ajout d'une sauce grâce à "ajouter une sauce"
@@ -32,7 +31,7 @@ exports.getOneSauce = (req, res, next) => {
   .catch(error => res.status(404).json({ error }));
 };
 
-// Gestion de la modification une sauce
+// Gestion de la modification d'une sauce
 exports.modifySauce = (req, res, next) => {
   const modifiedSauce = req.file ?
     { 
@@ -42,4 +41,49 @@ exports.modifySauce = (req, res, next) => {
   sauce.updateOne({_id: req.params.id}, {...modifiedSauce, _id:req.params.id})
   .then(() => res.status(200).json({message: 'Sauce modifiée !'}))
   .catch(error => res.status(400).json({ error }));
+};
+
+// Gestion du like ou du dislike d'une sauce
+exports.likeSauce = (req, res, next) => {
+  sauce.findOne({ _id: req.params.id})
+  .then(sauce => {
+    let message = "";
+    // Si l'utilisateur aime la sauce
+    if (req.body.like === 1)
+    {
+        sauce.usersLiked.push(req.body.userId);
+        message = "j\'aime";
+    }
+    // Si l'utilisateur n'aime pas la sauce
+    else if (req.body.like === -1)
+    {
+      sauce.usersDisliked.push(req.body.userId);
+      message = "je n\'aime pas";
+    }
+    else // Ici, l'utilisateur a supprimé son vote soit en enlevant son "j'aime" soit son "je n'aime pas"
+    {
+      // Recherche de l'id de l'utilisateur dans les utilisateurs qui aiment la sauce
+      let userIndex = sauce.usersLiked.findIndex(element => (element === req.body.userId));
+      if (userIndex !== -1) // -1 signifie que l'id n'a pas été trouvé dans le tableau
+      {
+        // Il a supprimé son "j'aime"
+        sauce.usersLiked.splice(userIndex, 1); // Suppression d'un élément à l'indice userIndex
+      }
+
+      // Recherche de l'id de l'utilisateur dans les utilisateurs qui n'aiment pas la sauce
+      userIndex = sauce.usersDisliked.findIndex(element => (element === req.body.userId));
+      if (userIndex !== -1)
+      {
+        // Il a supprimé son "je n'aime pas"
+        sauce.usersDisliked.splice(userIndex, 1); // Suppression d'un element a l'indice userIndex
+      }
+      message = "supression du précédent choix";
+    }
+    sauce.likes = sauce.usersLiked.length;
+    sauce.dislikes = sauce.usersDisliked.length;
+    sauce.save()
+    .then(() => res.status(201).json(message))
+    .catch(error => res.status(400).json({ error }));
+  })
+  .catch(error => res.status(400).json({ error }))
 };
--- models/sauce.js
+++ models/sauce.js
@@ -1,12 +1,16 @@
 const mongoose = require ('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    name: { type:String},
-    manufacturer: { type:String},
-    description: { type:String},
-    mainPepper: { type:String},
-    imageUrl: { type:String},
-    heat: { type:Number}
+    name: { type:String, required:true},
+    manufacturer: { type:String, required:true},
+    description: { type:String, required:true},
+    mainPepper: { type:String, required:true},
+    imageUrl: { type:String, required:true},
+    heat: { type:Number, required:true},
+    likes: { type:Number},
+    dislikes: { type:Number},
+    usersLiked: [{ type:String}],
+    usersDisliked: [{ type:String}]
 });
 
 module.exports = mongoose.model('sauce', sauceSchema);
